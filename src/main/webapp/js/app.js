db792ece8b7ec3e57f2a5ba918459f6053d2ad1c
Patrick Burke <hi@pburke.de>
2015-04-28 22:20:54 +0200

    Die anonyme Wrapper-Funktion entfernt, verwirrt nur.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,55 +1,53 @@
-+function ($) {
-    var connection = new WebSocket('ws://localhost:8080/socket'),
-        $timeline = $('#timeline'),
-        $message = $('#message');
+var connection = new WebSocket('ws://localhost:8080/socket'),
+    $timeline = $('#timeline'),
+    $message = $('#message');
 
-    /**
-     * Wird aufgerufen, wenn der Versuch eine Verbindung zum Server herzustellen (s.o.: "... new WebSocket...") erfolgreich war.
-     */
-    connection.onopen = function () {
-        $timeline.append('<li>[Verbindung hergestellt. Öffne einen zweiten Browser um mit dir selbst zu reden.]</li>')
-    };
+/**
+ * Wird aufgerufen, wenn der Versuch eine Verbindung zum Server herzustellen (s.o.: "... new WebSocket...") erfolgreich war.
+ */
+connection.onopen = function () {
+    $timeline.append('<li>[Verbindung hergestellt. Öffne einen zweiten Browser um mit dir selbst zu reden.]</li>')
+};
 
-    /**
-     * Tritt ein Fehler beim Verbinden mit dem Server oder danach auf, wird diese Funktion aufgerufen.
-     */
-    connection.onerror = function () {
-        $timeline.append('<li>[Es ist ein Fehler aufgetreten. Lade die Seite, falls das nicht hilft starte den Server neu und probiere es erneut.]</li>')
-    };
+/**
+ * Tritt ein Fehler beim Verbinden mit dem Server oder danach auf, wird diese Funktion aufgerufen.
+ */
+connection.onerror = function () {
+    $timeline.append('<li>[Es ist ein Fehler aufgetreten. Lade die Seite, falls das nicht hilft starte den Server neu und probiere es erneut.]</li>')
+};
 
-    /**
-     * Wird ausgeführt wenn der Browser eine Nachricht vom Server bekommt. Die gesendeten Daten (e.data) werden als
-     * Listenelement in die Liste in index.jsp eingetragen.
-     * @param e
-     */
-    connection.onmessage = function (e) {
-        $timeline.append('<li>' + e.data + '</li>');
-    };
+/**
+ * Wird ausgeführt wenn der Browser eine Nachricht vom Server bekommt. Die gesendeten Daten (e.data) werden als
+ * Listenelement in die Liste in index.jsp eingetragen.
+ * @param e
+ */
+connection.onmessage = function (e) {
+    $timeline.append('<li>' + e.data + '</li>');
+};
 
-    /**
-     * Hier wird alle 2 Sekunden ein "Ping" (irgendeine Nachricht ohne besonderen Inhalt) an den Server geschickt,
-     * um ihm zu zeigen das der Browser noch offen ist. Ansonsten würde der Server die Verbindung nach einem bestimmten
-     * Timeout (siehe ChatServlet.java) trennen.
-     */
-    setInterval(function() {
-        connection.send('---ping');
-    }, 2000);
+/**
+ * Hier wird alle 2 Sekunden ein "Ping" (irgendeine Nachricht ohne besonderen Inhalt) an den Server geschickt,
+ * um ihm zu zeigen das der Browser noch offen ist. Ansonsten würde der Server die Verbindung nach einem bestimmten
+ * Timeout (siehe ChatServlet.java) trennen.
+ */
+setInterval(function() {
+    connection.send('---ping');
+}, 2000);
 
-    /**
-     * Bei einem Klick auf den Senden Button (id="send" in index.jsp) wird die Nachricht aus dem Testfeld an den Server
-     * gesendet und anschließend das Textfeld geleert.
-     */
-    $('#send').on('click', function(e) {
-        connection.send($message.val());
+/**
+ * Bei einem Klick auf den Senden Button (id="send" in index.jsp) wird die Nachricht aus dem Testfeld an den Server
+ * gesendet und anschließend das Textfeld geleert.
+ */
+$('#send').on('click', function(e) {
+    connection.send($message.val());
 
-        $message.val('');
-    });
+    $message.val('');
+});
 
-    /**
-     * Drückt man im Textfeld die Enter-Taste wird ein Klick auf den Senden Button simuliert (e.which enhält immer den
-     * Code der Taste die gerdrückt wurde, 13 = Enter).
-     */
-    $message.on('keypress', function(e) {
-       if (e.which == 13) $('#send').click();
-    });
-}(jQuery);
+/**
+ * Drückt man im Textfeld die Enter-Taste wird ein Klick auf den Senden Button simuliert (e.which enhält immer den
+ * Code der Taste die gerdrückt wurde, 13 = Enter).
+ */
+$message.on('keypress', function(e) {
+   if (e.which == 13) $('#send').click();
+});
