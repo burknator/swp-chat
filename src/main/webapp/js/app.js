f0518e0f4539837eae60ba95592ebf4b9d3675b4
Patrick Burke <hi@pburke.de>
2015-04-30 22:49:20 +0200

    Du siehst diesen Commit nicht...

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,5 +1,5 @@
 // Das wir hier "/socket" verwenden, wird in ChatServlet.java festgelegt.
-var connection = new WebSocket('ws://' + window.location.href.replace(/http/, 'ws') + '/socket'),
+var connection = new WebSocket('ws://' + window.location.href.replace(/http/, 'ws').replace(/\/$/, '') + '/socket'),
     $timeline = $('#timeline'),
     $message = $('#message');
 
